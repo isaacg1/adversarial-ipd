2f9e742c2d3b7b9186b5456818c1fa986f32e95b
Isaac Grosof <isaacbg227@gmail.com>
2023-10-24 18:48:18 -0400

    Adversarial Iterated Prisoner's Dilemma: Intial Commit

---
+++ adversarial-game.py
@@ -0,0 +1,116 @@
+MOVES = 100
+FLIP_BUDGET = 40
+
+
+def swap(play):
+    if play == "c":
+        return "d"
+    return "c"
+
+
+def score(prisoners, flippers):
+    prisoner_scores = [0 for _ in prisoners]
+    flipper_scores = [0 for _ in prisoners]
+    for (index1, prisoner1) in enumerate(prisoners):
+        for (index2, prisoner2) in enumerate(prisoners):
+            for (index_f, flipper) in enumerate(flippers):
+                p1_score = 0
+                p1_state = []
+                p1_plays = []
+                p1_flipped = []
+
+                p2_score = 0
+                p2_state = []
+                p2_plays = []
+                p2_flipped = []
+
+                f_score = 0
+                f_state = []
+                flips_remaining = FLIP_BUDGET
+                for _ in range(MOVES):
+                    p1_play = prisoner1(p1_plays, p2_flipped, p1_state)
+                    p2_play = prisoner2(p2_plays, p1_flipped, p2_state)
+                    p1_plays.append(p1_play)
+                    p2_plays.append(p2_play)
+                    f_play = 0
+                    if flips_remaining > 0:
+                        f_play = flipper(
+                            p1_plays,
+                            p1_flipped,
+                            p2_plays,
+                            p2_flipped,
+                            flips_remaining,
+                            f_state,
+                        )
+                    if f_play == 3 and flips_remaining == 1:
+                        f_play = 1
+                    if p1_play == "c":
+                        p2_score += 2
+                    else:
+                        p1_score += 1
+                        f_score += 1
+                    if p2_play == "c":
+                        p1_score += 2
+                    else:
+                        p2_score += 1
+                        f_score += 1
+                    p1_flip = p1_play if f_play % 2 == 0 else swap(p1_play)
+                    p2_flip = p2_play if f_play < 2 else swap(p2_play)
+
+                    p1_flipped.append(p1_flip)
+                    p2_flipped.append(p2_flip)
+                    flips_remaining -= (f_play + 1) // 2
+                    assert(flips_remaining >= 0)
+                if False: # Debug information
+                    print(prisoner1.__name__, prisoner2.__name__, flipper.__name__)
+                    print(p1_score, p2_score, f_score, flips_remaining)
+                prisoner_scores[index1] += p1_score
+                prisoner_scores[index2] += p2_score
+                flipper_scores[index_f] += f_score
+    return (prisoner_scores, flipper_scores)
+
+
+def print_scores(scores, prisoners, flippers):
+    prisoner_scores, flipper_scores = scores
+    print("Prisoners:")
+    sorted_scores = sorted(zip(prisoners, prisoner_scores), key=lambda p: -p[1])
+    for prisoner, total_score in sorted_scores:
+        average_score = total_score / (2 * len(prisoners) * len(flippers))
+        print("{}: {:.6}".format(prisoner.__name__, average_score))
+    print("\nFlippers:")
+    sorted_scores = sorted(zip(flippers, flipper_scores), key=lambda p: -p[1])
+    for flipper, total_score in sorted_scores:
+        average_score = total_score / (len(prisoners) ** 2)
+        print("{}: {:.6}".format(flipper.__name__, average_score))
+
+
+if __name__ == "__main__":
+    from basic import (
+        basic_cooperate,
+        basic_defect,
+        basic_tit_for_tat,
+        basic_tit_for_two_tats,
+        basic_threshold,
+        basic_steady_flipper,
+        basic_random_flipper,
+        basic_immediate_flipper,
+        basic_non_flipper,
+        basic_biased_flipper,
+    )
+
+    prisoners = [
+        basic_cooperate,
+        basic_defect,
+        basic_tit_for_tat,
+        basic_tit_for_two_tats,
+        basic_threshold,
+    ]
+    flippers = [
+        basic_steady_flipper,
+        basic_random_flipper,
+        basic_immediate_flipper,
+        basic_non_flipper,
+        basic_biased_flipper,
+    ]
+    scores = score(prisoners, flippers)
+    print_scores(scores, prisoners, flippers)
---
+++ basic.py
@@ -0,0 +1,89 @@
+# Prisoners
+def basic_tit_for_tat(my_plays, their_plays, state):
+    if len(their_plays) == 0:
+        return "c"
+    return their_plays[-1]
+
+
+def basic_tit_for_two_tats(my_plays, their_plays, state):
+    if len(their_plays) < 2:
+        return "c"
+    if their_plays[-2:] == ["d", "d"]:
+        return "d"
+    return "c"
+
+
+def basic_cooperate(my_plays, their_plays, state):
+    return "c"
+
+
+def basic_defect(my_plays, their_plays, state):
+    return "d"
+
+
+def basic_threshold(my_plays, their_flipped_plays, state):
+    if len(their_flipped_plays) < 10:
+        return "c"
+    opp_c_freq = their_flipped_plays.count("c") / len(their_flipped_plays)
+    if opp_c_freq > 0.6:
+        return "c"
+    else:
+        return "d"
+
+
+# Flippers
+def basic_steady_flipper(
+    p1_moves, p1_flipped_moves, p2_moves, p2_flipped_moves, flips_left, state
+):
+    turn = len(p1_flipped_moves)
+    if turn % 5 == 0 and p1_moves[-1] == "c":
+        return 1
+    if turn % 5 == 2 and p2_moves[-1] == "c":
+        return 2
+    return 0
+
+
+def basic_random_flipper(
+    p1_moves, p1_flipped_moves, p2_moves, p2_flipped_moves, flips_left, state
+):
+    old_random = 1 if len(state) == 0 else state[0]
+    # Xorshift 32
+    new_random = old_random
+    new_random ^= (new_random << 13) % 2 ** 32
+    new_random ^= new_random >> 17
+    new_random ^= (new_random << 5) % 2 ** 32
+    if len(state) == 0:
+        state.append(new_random)
+    else:
+        state[0] = new_random
+    flip1 = new_random % 5 == 0
+    flip2 = (new_random % 25) // 5 == 0
+    out = 0
+    if flip1:
+        out += 1
+    if flip2:
+        out += 2
+    return out
+
+
+def basic_immediate_flipper(
+    p1_moves, p1_flipped_moves, p2_moves, p2_flipped_moves, flips_left, state
+):
+    out = 0
+    if p1_moves[-1] == "c":
+        out += 1
+    if p2_moves[-1] == "c":
+        out += 2
+    return out
+
+
+def basic_non_flipper(
+    p1_moves, p1_flipped_moves, p2_moves, p2_flipped_moves, flips_left, state
+):
+    return 0
+
+
+def basic_biased_flipper(
+    p1_moves, p1_flipped_moves, p2_moves, p2_flipped_moves, flips_left, state
+):
+    return 1
